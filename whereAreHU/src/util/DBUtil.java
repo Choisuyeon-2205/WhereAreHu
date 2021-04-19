ab0238c89526765014ccc2073cd643b00617892d
suyeon <suyeon2205@naver.com>
2021-04-19 17:49:35 +0900

    프로젝트 설정

---
+++ whereAreHU/src/util/DBUtil.java
@@ -0,0 +1,65 @@
+package util;
+
+import java.sql.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBUtil {
+	
+	//1. DB연결을 만들어주기
+	/*
+	public static Connection getConnection() {
+		Connection conn = null;
+		String driverName ="oracle.jdbc.driver.OracleDriver";
+		String url="jdbc:oracle:thin:@localhost:1521:xe";
+		String userid="hr", password="hr";
+		
+		try {
+			Class.forName(driverName);
+			conn= DriverManager.getConnection(url,userid,password);
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return conn;
+	}
+	*/
+	
+	//1. DB연결을 만들어주기(connection pool)
+		public static Connection getConnection() {
+			Connection conn = null;
+			Context initContext;
+			try {
+				initContext = new InitialContext();
+				Context envContext  = (Context)initContext.lookup("java:/comp/env");
+				DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+				try {
+					conn = ds.getConnection();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (NamingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return conn;
+		}
+	
+	//2. 자원반납
+	public static void dbClose(ResultSet rs, Statement st, Connection conn) {
+		try {
+			if(rs!=null) rs.close();
+			if(st!=null) st.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
